8183c420913ff18a691c273dc47fcfba1a7a244b
roger.santos <roger.rezende@gmail.com>
2022-04-02 21:55:11 -0300

    criação do teste para validar se método para verificar uma pessoa é maior de idade está ok

--- src/test/java/one/innovation/digital/junit/PessoaTeste.java
+++ src/test/java/one/innovation/digital/junit/PessoaTeste.java
@@ -8,7 +8,25 @@ import java.time.LocalDate;
 public class PessoaTeste {
     @Test
     public void validarCalculoIdade() {
-        Pessoa pessoa = new Pessoa("Anakin", LocalDate.of(2020, 1, 1));
-        Assertions.assertEquals(2, pessoa.getIdade());
+        Pessoa anakin = new Pessoa("Anakin", LocalDate.of(2020, 1, 1));
+        Assertions.assertEquals(2, anakin.getIdade());
+    }
+
+    @Test
+    public void validarCalculoIdadeIncorreto() {
+        Pessoa leia = new Pessoa("Leia", LocalDate.of(2000, 1, 1));
+        Assertions.assertEquals(3, leia.getIdade());
+    }
+
+    @Test
+    public void deveRetornarMaiorDeIdade(){
+        Pessoa luke = new Pessoa("Luke", LocalDate.of(2000, 1, 1));
+        Assertions.assertTrue(luke.eMaiorDeIdade());
+    }
+
+    @Test
+    public void deveRetornarMenorDeIdade(){
+        Pessoa han = new Pessoa("Han", LocalDate.of(2021, 1, 1));
+        Assertions.assertTrue(han.eMaiorDeIdade());
     }
 }
